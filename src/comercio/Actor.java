384e307fae9567494cc9cb822d95ca1911cb327a
AnaliaFc <70965636+AnaliaFc@users.noreply.github.com>
2020-09-14 11:25:11 -0300

    Update Actor.java

--- src/comercio/Actor.java
+++ src/comercio/Actor.java
@@ -1,5 +1,40 @@
 package comercio;
 
 public abstract class Actor {
+  
+  protected int id;
+	protected Contacto contacto;
+	
+	public Actor(int id, Contacto contacto)
+	{
+		setId(id);
+		setContacto(contacto);
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Contacto getContacto() {
+		return contacto;
+	}
+	
+	public void setContacto(Contacto contacto) {
+		this.contacto = contacto;
+	}
+	
+	public boolean equals(Actor act)
+	{
+		return this.id==act.id;
+	}
+
+	@Override
+	public String toString() {
+		return "id=" + id + contacto.toString();
+	}
+	
 
 }

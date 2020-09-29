b4be8454933dded611fde1cf4b441172431da548
Ezequiel de la Fuente <ezequieldelafuente2001@gmail.com>
2020-09-29 17:04:18 -0300

    No me la reconocia y ahora si.

--- src/test/TestDiaRetiro.java
+++ src/test/TestDiaRetiro.java
@@ -1,15 +1,13 @@
 package test;
-
 import java.time.LocalTime;
-
 import comercio.DiaRetiro;
 
 public class TestDiaRetiro {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 		//dia de la semana, hora desde, hora hasta, intervalo.
-		DiaRetiro diaretiro1 = new DiaRetiro(5,LocalTime.of( 9, 30), LocalTime.of( 20, 30),30);
+		// DiaRetiro diaretiro1 = new DiaRetiro(5,LocalTime.of( 9, 30), LocalTime.of( 20, 30),30);
+		DiaRetiro diaretiro1 = new DiaRetiro(5, LocalTime.of(9, 30),LocalTime.of(20,30), 30);
 		DiaRetiro diaretiro2 = new DiaRetiro(7,LocalTime.of( 8, 30), LocalTime.of( 21, 30),50);
 		
 		System.out.println(diaretiro1);
@@ -39,7 +37,5 @@ public class TestDiaRetiro {
 			System.out.println(e.getMessage());
 		}
 	
-
-	}
-
+    }
 }

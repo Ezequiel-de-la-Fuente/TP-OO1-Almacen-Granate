f31e78eac5b6f2f4c94877ffc42098fd594fa5ea
Lucas Brieva <lucasbrieva@gmail.com>
2020-10-02 21:30:13 -0300

    Modificado Test de Articulo

--- src/test/TestArticulo.java
+++ src/test/TestArticulo.java
@@ -5,7 +5,7 @@ import comercio.Articulo;
 public class TestArticulo {
 
 	public static void main(String[] args) {
-		Articulo articulo = new Articulo("Oreos", "111111111", 18);
+		Articulo articulo = new Articulo("Gallo Snack Salados", "7790070411365", 60);
 		System.out.println(articulo);
 		
 		try {
@@ -16,6 +16,18 @@ public class TestArticulo {
 		
 		try {
 			articulo.setPrecio(-100);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		try {
+			articulo.setCodBarras("1111112531"); //Código de barras menor a 13 digitos. 
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		try {
+			articulo.setCodBarras("1111112531123"); //Código de barras de 13 digitos pero invalido. 
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}

40037970fd76188350bb78611bed9351623141ac
AnaliaFc <70965636+AnaliaFc@users.noreply.github.com>
2020-10-10 17:27:08 -0300

    Update TestC16.java

--- src/test/CU/TestC16.java
+++ src/test/CU/TestC16.java
@@ -11,11 +11,12 @@ public class TestC16 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 
-      Envio envio1 = new Envio(LocalDate.now(), true, LocalTime.now(), LocalTime.now(), 8, new Ubicacion(-34.8080732,-58.3981827));
-		  envio1.setCosto(new Ubicacion(-34.8285678,-58.3915826), 400, 8);
-		  System.out.println("Costo Fijo $400 y Costo por Km $8");
-		  System.out.println("Las locaciones se encuentran a un poco mas de 2km");
-		  System.out.println("Costo de envio es: " + envio1.getCosto());
+     		Envio envio1 = new Envio(LocalDate.now(), true, LocalTime.of(20, 00, 00), LocalTime.of(18, 00, 00), 8, new Ubicacion(-34.8080732,-58.3981827));
+          	System.out.println(envio1);
+          	envio1.setCosto(new Ubicacion(-34.8285678,-58.3915826), 400, 8);
+		System.out.println("Costo Fijo $400 y Costo por Km $8");
+		System.out.println("Las locaciones se encuentran a un poco mas de 2km");
+		System.out.println("Costo de envio es: " + envio1.getCosto());
 			
 	}
 

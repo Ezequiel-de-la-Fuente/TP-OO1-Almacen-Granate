9a6702f01ff672424d2ab86a7e62a5ceb22aaac7
AnaliaFc <70965636+AnaliaFc@users.noreply.github.com>
2020-10-10 19:23:06 -0300

    Update TestC16.java

--- src/test/CU/TestC16.java
+++ src/test/CU/TestC16.java
@@ -13,7 +13,7 @@ public class TestC16 {
 		 
      		Envio envio1 = new Envio(LocalDate.now(), true, LocalTime.of(20, 00, 00), LocalTime.of(18, 00, 00), 8, new Ubicacion(-34.8080732,-58.3981827));
           	System.out.println(envio1);
-          	envio1.setCosto(new Ubicacion(-34.8285678,-58.3915826), 400, 8);
+          	envio1.setCosto(new Ubicacion(-34.8255367,-58.390943), 400, 8);
 		System.out.println("Costo Fijo $400 y Costo por Km $8");
 		System.out.println("Las locaciones se encuentran a un poco mas de 2km");
 		System.out.println("Costo de envio es: " + envio1.getCosto());

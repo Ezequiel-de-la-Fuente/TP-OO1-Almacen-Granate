edb1c90678cdc0bbeeb700f942a57d4428f53255
AnaliaFc <70965636+AnaliaFc@users.noreply.github.com>
2020-10-10 18:07:44 -0300

    Update TestC17.java

--- src/test/CU/TestC17.java
+++ src/test/CU/TestC17.java
@@ -14,15 +14,16 @@ public class TestC17 {
 		Comercio almacen = new Comercio(24,contactoAlmacen,"Granate-Store","30-52745070-1",100,15.50,28,3,5);
 		Contacto contactoCliente = new Contacto("anafc@gmail.com", "11 3030 3456", new Ubicacion(28, 10));
 		Cliente cliente1=new Cliente(1,contactoCliente,"Fernandez","Ana",36500548);
-    System.out.println("Ubicacion del almacen: "+almacen.traerUbicacion());
+    		System.out.println("Ubicacion del almacen: "+almacen.traerUbicacion());
 		System.out.println("Ubicacion del cliente: "+cliente1.traerUbicacion());
 		
-    try {
+    		try {
 			 almacen.setContacto(null);
 			 almacen.traerUbicacion();
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+		
 		try {
 			 cliente1.setContacto(null);
 			 cliente1.traerUbicacion();

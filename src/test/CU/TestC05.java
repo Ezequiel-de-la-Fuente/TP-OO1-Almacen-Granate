a42527ff2d1e70250b5b9f757d011318f87a94a6
Maxiabbru <71236808+Maxiabbru@users.noreply.github.com>
2020-10-13 01:58:35 -0300

    Update TestC05.java

--- src/test/CU/TestC05.java
+++ src/test/CU/TestC05.java
@@ -2,15 +2,10 @@ package test.CU;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
-
-import comercio.Articulo;
-import comercio.Carrito;
 import comercio.Cliente;
 import comercio.Comercio;
 import comercio.Contacto;
-
 import comercio.RetiroLocal;
 import comercio.Turno;
 import comercio.Ubicacion;
@@ -24,43 +19,44 @@ public class TestC05 {
 		RetiroLocal retiroLocal1 = new RetiroLocal(LocalDate.now(), true, LocalTime.of(15,30));
 		RetiroLocal retiroLocal2 = new RetiroLocal(LocalDate.now(), true, LocalTime.of(10,30));
 		RetiroLocal retiroLocal3 = new RetiroLocal(LocalDate.now(), true, LocalTime.of(12,30));
-		
+	
 		Contacto contacto = new Contacto("almacen@gmail.com", "15 4567 7894", new Ubicacion(31, 12));
+		
 		Comercio almacen = new Comercio(24,contacto,"Granate-Store","30-52745070-1",100,15.50,28,3,5);
 		
 		Cliente cliente = new Cliente(1, contacto, "Abbruzzese","Maximiliano", 38154177);
-		Articulo pepas=new Articulo("Pepas Pig","7791762131394" , 9.50);
-		Carrito carrito1 = new Carrito (LocalDate.now(), LocalTime.now(), false, 20.50,cliente,retiroLocal1, 1);
-		Carrito carrito2 = new Carrito (LocalDate.now(), LocalTime.now(), false, 20.50,cliente,retiroLocal2, 2);
-		Carrito carrito3 = new Carrito (LocalDate.now(), LocalTime.now(), false, 20.50,cliente,retiroLocal3, 3);
-		ArrayList<Carrito> listaCarritos = new ArrayList<Carrito>();
+		Cliente cliente1 = new Cliente(2, contacto, "Brieva","Lucas", 39664602);
+		Cliente cliente2 = new Cliente(3, contacto, "De La Fuente","Ezequiel", 42568324);
 		LocalDate dia = null;
 		
 		try {
-			carrito1.agregarItem(pepas, 3);
-			listaCarritos.add(carrito1);
-			listaCarritos.add(carrito2);
-			listaCarritos.add(carrito3);
-			almacen.setLstCarrito(listaCarritos);
+			
+			almacen.agregarCarrito(LocalDate.now(), LocalTime.now(), false, 20.50, retiroLocal1, cliente);
+			almacen.agregarCarrito(LocalDate.now(), LocalTime.now(), false, 20.50, retiroLocal2, cliente1);
+			almacen.agregarCarrito(LocalDate.now(), LocalTime.now(), false, 20.50, retiroLocal3, cliente2);
 			//System.out.println(almacen.getLstCarrito());
 		}
+		
 		catch (Exception e) {
 			System.out.println(e.getMessage());
 
 		}
+		
 		try {
-			almacen.agregarDiaRetiro(5, LocalTime.of(9, 30),LocalTime.of(20,30), 30);
-			almacen.agregarDiaRetiro(1,LocalTime.of( 8, 30), LocalTime.of( 21, 30),25);
-			almacen.agregarDiaRetiro(2,LocalTime.of( 7, 30), LocalTime.of( 19, 30),10);
-			almacen.agregarDiaRetiro(3,LocalTime.of( 8, 00), LocalTime.of( 21, 00),20);
-			almacen.agregarDiaRetiro(4,LocalTime.of( 9, 00), LocalTime.of( 22, 00),15);
-			almacen.agregarDiaRetiro(6,LocalTime.of( 10, 30), LocalTime.of( 22, 30),40);
-			almacen.agregarDiaRetiro(7,LocalTime.of( 8, 30), LocalTime.of( 21, 30),30);
+			
+			almacen.agregarDiaRetiro(1,LocalTime.of( 8, 30), LocalTime.of( 21, 00),30);
+			almacen.agregarDiaRetiro(2,LocalTime.of( 8, 30), LocalTime.of( 21, 00),30);
+			almacen.agregarDiaRetiro(3,LocalTime.of( 8, 30), LocalTime.of( 21, 00),30);
+			almacen.agregarDiaRetiro(4,LocalTime.of( 8, 30), LocalTime.of( 22, 00),30);
+			almacen.agregarDiaRetiro(5,LocalTime.of( 8, 30), LocalTime.of(21, 00), 30);
+			almacen.agregarDiaRetiro(6,LocalTime.of( 10, 00), LocalTime.of( 15, 00),30);
+			almacen.agregarDiaRetiro(7,LocalTime.of( 10, 00), LocalTime.of( 15, 00),30);
 						
 			//System.out.println(almacen.getLstDiaRetiro());
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+		
 		
 		try {
 			//while(almacen.traerHoraRetiro(LocalDate.now())!= null) {
@@ -77,12 +73,12 @@ public class TestC05 {
 				System.out.println("Hora:"  + turno.getHoraTurno() + " \nEstado:Ocupado\n");
 			}
 			}
-			//}
+		
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 		
 
-	}
+	} // cierro el main
 
-}
+} //cierro la clase test
